9c0b9a54d1498129a276d96d3b5a1fb813b90aa7
huangly <huangly@akulaku.com>
2021-03-15 16:29:26 +0800

    fix: 优化单个选项元素的高度的计算

--- src/components/ScrollPicker/index.js
+++ src/components/ScrollPicker/index.js
@@ -1,12 +1,13 @@
-
 import React, { useRef, useEffect, memo, useCallback } from "react";
-import { debounce } from '../../utils'
+import { debounce } from "../../utils";
 
 import "./styles.css";
 
 const ScrollPicker = memo(({ data = [], value, onChange, ...props }) => {
   const contentRef = useRef();
-  const itemRef = useRef();
+  const containerRef = useRef();
+  // 单个选项元素的高度
+  const itemHeight = useRef(50);
   // 记录拖动开始的纵坐标
   const lastStartY = useRef(0);
 
@@ -23,26 +24,32 @@ const ScrollPicker = memo(({ data = [], value, onChange, ...props }) => {
   }, []);
 
   const handleTouchEnd = useCallback(() => {
-    // 获取一个单位的高度
-    const itemHeight = itemRef.current.offsetHeight;
     // 四舍五入知道当前 index
-    const index = Math.round(contentRef.current.scrollTop / itemHeight);
+    const index = Math.round(contentRef.current.scrollTop / itemHeight.current);
     // 吸附居中
-    contentRef.current.scrollTop = index * itemHeight;
+    contentRef.current.scrollTop = index * itemHeight.current;
     // 触发 onChange
     if (onChange) {
       onChange(data[index].value, data[index]);
     }
   }, [onChange, data]);
 
+  
+  useEffect(() => {
+    // 单个选项元素的高度 = 容器总高度 / （元素长度 + 前后空元素)
+    itemHeight.current = Number(containerRef.current.offsetHeight / (data.length + 2)).toFixed(2);
+  }, [data])
+
   // 如果没有值，就设定第一个为默认值
   useEffect(() => {
+    // 设定初始值
     if (!value && onChange) {
       onChange(data[0].value, data[0]);
     }
     // 仅初始化才调用，不需要填依赖项
     // eslint-disable-next-line
   }, []);
+
 
   return (
     <div className="scroll-picker" {...props}>
@@ -55,18 +62,20 @@ const ScrollPicker = memo(({ data = [], value, onChange, ...props }) => {
         onTouchEnd={handleTouchEnd}
         className="scroll-picker-content"
       >
-        <div ref={itemRef} className="scroll-picker-content-item">
-          &nbsp;
-        </div>
-        {data.map((item) => (
-          <div key={item.value} className="scroll-picker-content-item">
-            {item.label}
+        <div ref={containerRef}>
+          <div className="scroll-picker-content-item">
+            &nbsp;
           </div>
-        ))}
-        <div className="scroll-picker-content-item">&nbsp;</div>
+          {data.map((item) => (
+            <div key={item.value} className="scroll-picker-content-item">
+              {item.label}
+            </div>
+          ))}
+          <div className="scroll-picker-content-item">&nbsp;</div>
+        </div>
       </div>
     </div>
   );
 });
 
-export default ScrollPicker
+export default ScrollPicker;
